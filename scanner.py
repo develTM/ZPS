c39d409e4c731f3300c174dea818b14fd11afa7a
develTM <tm.maciejak@student.uw.edu.pl>
2020-11-05 21:45:23 +0100

    wojna dla wielu graczy, plan scannera

---
+++ scanner.py
@@ -0,0 +1,13 @@
+
+
+#convert image to bpm
+#locate card,(might prefer to use monochrome background):
+#a Read in image and walnut.
+#b Take any pixel of the walnut.
+#c Find all pixels of the image with the same color.
+#d Check to see if the surrounding pixels coincide with the surrounding pixels of the card (and break as soon as you find one mismatch to minimize time).
+
+#https://towardsdatascience.com/image-recognition-with-machine-learning-on-python-convolutional-neural-network-363073020588
+#resize image, may convert to black-white
+#recognize color
+#recognize value
